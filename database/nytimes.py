490d53e3f48fac21e51bde650b3f45bed14d441b
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-06-02 21:32:06 +0530

    finalized nytiimes.py

--- database/nytimes.py
+++ database/nytimes.py
@@ -4,57 +4,47 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
+import csv
 
-def fetch_bbc_headlines(pages=5):
-    headline=[]
-    date=[]
-    articles = []
-    url = "https://www.nytimes.com/spotlight/india"
+def scrape_data(url):
+    # Set up the Chrome webdriver
+    driver = webdriver.Chrome() 
     
-    # Set up the Selenium WebDriver (using Chrome in this example)
-    driver = webdriver.Chrome()
+    # Navigate to the URL
+    driver.get(url)
+    
+    # Parse the HTML content
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    
+    # Find all headline elements
+    headlines = soup.find_all('h3', class_='css-1j88qqx e15t083i0')
+    
+    # Find all date elements
+    dates = soup.find_all('div', class_='css-e0xall e15t083i3')
+    
+    # Extracting headlines and dates
+    headlines_list = [headline.text.strip() for headline in headlines]
+    dates_list = [date.text.strip() for date in dates]
+    
+    # Close the driver
+    driver.quit()
+    
+    return headlines_list, dates_list
 
-    try:
-        driver.get(url)
-        time.sleep(3)  # Wait for the page to load (you can adjust the sleep time)
-        
-       
-        for page in range(pages):
-            # Use WebDriverWait to wait until the news items are loaded
-            WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located((By.TAG_NAME, "h3"))
-            )
-            
-            soup = BeautifulSoup(driver.page_source, 'html.parser')
-            # Extract headlines
-            # print(soup.find_all('h2', class_='sc-2c72d884-3 fWWpXO'))
-            for article in soup.find_all('h3', class_='css-1j88qqx e15t083i0'):
-                headline.append(article.text.strip())
-            # print(soup.find_all('span', class_='sc-df20d569-1 fbRULV'))
-            for dates in soup.find_all("span", class_="", attrs={"data-testid": "todays-date"}):
-                if dates:
-                    date.append( dates.get_text(strip=True))
-            # for item in soup.find_all('div', class_='sc-da05643e-0 kbaPPZ'):
-            #     if item:
-            #         headlines.append(item.text.strip())
-            
-            # Click on the next page button
-            # next_page_button = driver.find_element(By.CSS_SELECTOR, "button[data-testid='pagination-next-button']")
-            # next_page_button.click()
-            # time.sleep(15)  # Wait for the new page to load # Wait for the new page to load
-        
-    finally:
-        driver.quit()
-        
+def save_to_csv(headlines, dates):
+    with open('headlines.csv', 'a', newline='', encoding='utf-8') as file:
+        writer = csv.writer(file)
+        print("Number of headlines:", len(headlines))
+        for headline, date in zip(headlines, dates):
+            print("Appending headline:", headline)
+            writer.writerow(['nytimes', headline, date])
 
-    return (headline,date)
+def main():
+    # Change the range according to the number of pages you want to scrape
+    for page_number in range(1, 4):  # Scraping 3 pages in this example
+        url = f'https://www.nytimes.com/spotlight/india?page={page_number}'
+        headlines, dates = scrape_data(url)
+        save_to_csv(headlines, dates)
 
 if __name__ == "__main__":
-    headline,date = fetch_bbc_headlines(pages=1)
-    # print first headline than corresponding date in loop
-    # print(headline)
-    # print(date)
-    for i in range(len(headline)):
-        print(headline[i], date[i])
-    
-    
+    main()
