39bb9241831eb4615b0acc9c0ba52887c04f1fd4
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-06-14 15:27:37 +0530

    added full article scrapping to nytimes

--- database/nytimes.py
+++ database/nytimes.py
@@ -31,25 +31,51 @@ def scrape_data(url):
     headlines_list = [headline.text.strip() for headline in headlines]
     dates_list = [date.text.strip() for date in dates]
     
+    # Close the driver for now
+    driver.quit()
+    
+    # Scrape articles
+    articles = []
+    for link in full_links:
+        articles.append(scrape_article(link))
+    
+    return headlines_list, dates_list, full_links, articles
+
+def scrape_article(url):
+    # Set up the Chrome webdriver
+    driver = webdriver.Chrome()
+    
+    # Navigate to the article URL
+    driver.get(url)
+    
+    # Parse the HTML content
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    
+    # Extract the article text
+    paragraphs = soup.find_all('p', class_='css-at9mc1 evys1bk0')
+    article_text = " ".join([para.text.strip() for para in paragraphs])
+
+    print(article_text)
+    
     # Close the driver
     driver.quit()
     
-    return headlines_list, dates_list, full_links
+    return article_text
 
-def save_to_csv(headlines, dates, links):
+def save_to_csv(headlines, dates, links, articles):
     with open('headlines.csv', 'a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
         print("Number of headlines:", len(headlines))
-        for headline, date, link in zip(headlines, dates, links):
+        for headline, date, link, article in zip(headlines, dates, links, articles):
             print("Appending headline:", headline)
-            writer.writerow(['nytimes', headline, date, link])
+            writer.writerow(['nytimes', headline, date, link, article])
 
 def main():
     # Change the range according to the number of pages you want to scrape
     for page_number in range(1, 4):  # Scraping 3 pages in this example
         url = f'https://www.nytimes.com/spotlight/india?page={page_number}'
-        headlines, dates, links = scrape_data(url)
-        save_to_csv(headlines, dates, links)
+        headlines, dates, links, articles = scrape_data(url)
+        save_to_csv(headlines, dates, links, articles)
 
 if __name__ == "__main__":
     main()
