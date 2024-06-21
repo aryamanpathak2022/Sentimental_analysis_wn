cbc2f6189d2a045a655ee088e6769f207e40f14c
aryamanpathak2022 <Aryaman.Pathak@iiitb.ac.in>
2024-06-22 01:09:32 +0530

    Update

--- database/cnn.py
+++ database/cnn.py
@@ -47,20 +47,27 @@ def fetch_bbc_headlines(pages=5):
             for date_element in soup.find_all('div', class_='container__date container_list-images-with-description__date inline-placeholder'):
                 dates.append(date_element.text.strip())
                 
-            for link_element in soup.find_all('span', class_='container__headline-text', href=True):
-                link =  link_element['href']
-                print(link_element['href'])
+            for link_element in soup.find_all('span', class_='container__headline-text'):
+                link = link_element.get('data-zjs-href')
+                print(link)
 
                 links.append(link)
 
             # Navigate to the next page if there is one
             try:
                 print("hello")
-                next_page_button = driver.find_element(By.CLASS_NAME, "pagination-arrow pagination-arrow-right search__pagination-link text-active")
+                next_page_button = driver.find_element(By.CLASS_NAME, "pagination-arrow-right")
+                print(next_page_button)
                 next_page_button.click()
                 time.sleep(15)  # Wait for the new page to load
             except:
-                print("error-hello")
+                print("error")
+                break  # If there is no next page, break the loop
+               
+                # print the error
+
+
+
                 break  # Exit the loop if there are no more pages
 
     finally:
@@ -68,13 +75,15 @@ def fetch_bbc_headlines(pages=5):
     
     # Scrape full articles
     print(dates)
+    print(links)
+    print(headlines)
     for link in range(len(links)):
         print(links[link])
         articles.append(scrape_article(links[link]))
         # save to csv
         with open('headlines.csv', 'a', newline='', encoding='utf-8') as file:
              writer = csv.writer(file)
-             writer.writerow(['bbc', headlines[link], dates[link], links[link], articles[link]])
+             writer.writerow(['cnn', headlines[link], dates[link], links[link], articles[link]])
 
     
     return headlines, dates, links, articles
@@ -107,7 +116,7 @@ def save_to_csv(headlines, dates, links, articles):
             writer.writerow(['cnn', headline, date, link, article])
 
 if __name__ == "__main__":
-    headlines, dates, links, articles = fetch_bbc_headlines(pages=15)
+    headlines, dates, links, articles = fetch_bbc_headlines(pages=145)
     save_to_csv(headlines, dates, links, articles)
     
     # Print the results for verification
